86db8b793c292685e16cbda35996d18bfc5f11de
Team 007 Community <88379378+T007Community@users.noreply.github.com>
2021-11-22 17:02:27 +0600

    update

--- nethunter.sh
+++ nethunter.sh
@@ -1,9 +1,25 @@
+function checkinternet() 
+{
+  ping -c 1 google.com > /dev/null 2>&1
+  if [[ "$?" != 0 ]]
+  then
+    echo -e $yellow " Checking For Internet: ${RedF}FAILED"
+    echo
+    echo -e $red "This Script Needs An Active Internet Connection"
+    echo
+    echo -e $yellow " setup Exit"
+    echo && sleep 2
+    exit
+  else
+    echo -e $yellow " Checking For Internet: ${LighGreenF}CONNECTED"
+  fi
+}
+checkinternet
+echo Kali install
 clear
-echo "#Install Kali Nethunter | Without Root"
-sleep 5
-clear
-echo ' Configuration Started'
-echo 'It will take a few hours'
+sleep 3
+echo Configuration Started
+echo It will take a few hours
 sleep 2
 termux-setup-storage
 apt update
